94feacb9c80bf87cd492e13087d2a5d59a74c1e4
Joao <joaoded@hotmail.com>
2019-02-06 01:10:12 -0200

    New editions

--- needy_institutions/needy_institutions/spiders/institutions_spider.py
+++ needy_institutions/needy_institutions/spiders/institutions_spider.py
@@ -7,10 +7,20 @@ class needyDivi(scrapy.Spider):
     start_urls = ['https://www.telelistas.net/mg/divinopolis/associacoes+beneficentes']
 
     def parse(self, response):
-      results = response.css('.text_resultado_ib a ::text').extract()
-      enderecos = response.css('.text_endereco_ib ::text').extract()
+      #results = response.css('.text_resultado_ib a ::text').extract()
+      #enderecos = response.css('.text_endereco_ib ::text').extract()
+      teste = response.css('#Content_Regs table')
+      name = scrapy.Field()
+      address = scrapy.Field()
       print('NEEDY')
-      for result in results:
+      for result in teste:
+        name = result.css('.text_resultado_ib a ::text').get(),
+        address = result.css('.text_endereco_ib ::text').get()
+        yield {
+          'name': name,
+          'address': address
+        }
+      ''' for result in results:
         print(result)
       for endereco in enderecos:
-        print(endereco)  
+        print(endereco) '''  
