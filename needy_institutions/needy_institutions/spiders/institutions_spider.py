9e26d623749cf6c77c117c8df30797cca8cd1b82
Joao <joaoded@hotmail.com>
2019-02-07 02:32:06 -0200

    Changing needyInstitutions crawler code

--- needy_institutions/needy_institutions/spiders/institutions_spider.py
+++ needy_institutions/needy_institutions/spiders/institutions_spider.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from unidecode import unidecode
 
 class needyDivi(scrapy.Spider):
     name = 'needyDivi'
     start_urls = ['https://www.telelistas.net/mg/divinopolis/associacoes+beneficentes']
 
     def parse(self, response):
-      #results = response.css('.text_resultado_ib a ::text').extract()
-      #enderecos = response.css('.text_endereco_ib ::text').extract()
-      teste = response.css('#Content_Regs table')
+      lista = response.css('#Content_Regs table')
       name = scrapy.Field()
-      address = scrapy.Field()
+      endereco = scrapy.Field()
       print('NEEDY')
-      for result in teste:
-        name = result.css('.text_resultado_ib a ::text').get(),
-        address = result.css('.text_endereco_ib ::text').get()
-        yield {
+      my_list = []
+      for result in lista:
+        name = result.css('.text_resultado_ib a::text').extract_first()
+        endereco = result.css('.text_endereco_ib ::text').extract_first()
+        my_list.append({'name': name, 'teste': endereco})
+        '''yield {
           'name': name,
-          'address': address
-        }
-      ''' for result in results:
-        print(result)
-      for endereco in enderecos:
-        print(endereco) '''  
+          'price': endereco
+        }'''
+      for e in my_list:
+        if e['name'] is not None and e['teste'] is not None :
+          e['name'] = unidecode(e['name'].strip())
+          e['teste'] = unidecode(e['teste'].strip())
+      print(my_list)    
