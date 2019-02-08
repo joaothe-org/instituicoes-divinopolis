6a683d45a0f94d4dfc7d1d8b52500ae6a5916a2e
Joao <joaoded@hotmail.com>
2019-02-08 01:29:41 -0200

    Sending fast refactoring

--- needy_institutions/needy_institutions/spiders/institutions_spider.py
+++ needy_institutions/needy_institutions/spiders/institutions_spider.py
@@ -7,34 +7,34 @@ class needyDivi(scrapy.Spider):
     start_urls = ['https://www.telelistas.net/mg/divinopolis/associacoes+beneficentes']
 
     def parse(self, response):
-      lista = response.css('#Content_Regs table')
+      _list = response.css('#Content_Regs table')
       name = scrapy.Field()
-      endereco = scrapy.Field()
+      address = scrapy.Field()
       print('NEEDY')
       my_list = []
-      for result in lista:
+      for result in _list:
         name = result.css('.text_resultado_ib a::text').extract_first()
-        endereco = result.css('.text_endereco_ib ::text').extract_first()
-        my_list.append({'name': name, 'teste': endereco})
-      for e in my_list:
-        if e['name'] is not None and e['teste'] is not None :
-          e['name'] = unidecode(e['name'].strip())
-          e['teste'] = unidecode(e['teste'].strip())
+        address = result.css('.text_endereco_ib ::text').extract_first()
+        my_list.append({'name': name, 'address': address})
+      for item in my_list:
+        if item['name'] is not None and item['address'] is not None :
+          item['name'] = unidecode(item['name'].strip())
+          item['address'] = unidecode(item['address'].strip())
 
-      def myFunc(x):
-        if x['name'] is None or x['teste'] is None:
-          return False
-        else:
-          return True
+    
+      filteredList = filter(self.myFunc, my_list)
+      print(self.Remove(filteredList)) 
 
-      filteredList = filter(myFunc, my_list)
-
-      def Remove(duplicate): 
-        final_list = [] 
-        for num in duplicate: 
-          if num not in final_list: 
-            final_list.append(num) 
-        return final_list 
-      
-      print(Remove(filteredList)) 
+    def myFunc(self, x):
+      if x['name'] is None or x['address'] is None:
+        return False
+      else:
+        return True
+    
+    def Remove(self, duplicate): 
+      final_list = [] 
+      for num in duplicate: 
+        if num not in final_list: 
+          final_list.append(num) 
+      return final_list 
     
