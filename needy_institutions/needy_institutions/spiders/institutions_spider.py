a7fe4d9224d30f76685018414e312acf6475036d
Joao <joaoded@hotmail.com>
2019-02-08 01:13:43 -0200

    Removing none type items and duplications from the list

--- needy_institutions/needy_institutions/spiders/institutions_spider.py
+++ needy_institutions/needy_institutions/spiders/institutions_spider.py
@@ -16,12 +16,25 @@ class needyDivi(scrapy.Spider):
         name = result.css('.text_resultado_ib a::text').extract_first()
         endereco = result.css('.text_endereco_ib ::text').extract_first()
         my_list.append({'name': name, 'teste': endereco})
-        '''yield {
-          'name': name,
-          'price': endereco
-        }'''
       for e in my_list:
         if e['name'] is not None and e['teste'] is not None :
           e['name'] = unidecode(e['name'].strip())
           e['teste'] = unidecode(e['teste'].strip())
-      print(my_list)    
+
+      def myFunc(x):
+        if x['name'] is None or x['teste'] is None:
+          return False
+        else:
+          return True
+
+      filteredList = filter(myFunc, my_list)
+
+      def Remove(duplicate): 
+        final_list = [] 
+        for num in duplicate: 
+          if num not in final_list: 
+            final_list.append(num) 
+        return final_list 
+      
+      print(Remove(filteredList)) 
+    
