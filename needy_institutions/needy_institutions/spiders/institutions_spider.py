cfba55a2f68d89ed4fe9f71311a015c003a3f1b8
Joao <joaoded@hotmail.com>
2019-02-28 16:50:12 -0300

    Add next page support

--- needy_institutions/needy_institutions/spiders/institutions_spider.py
+++ needy_institutions/needy_institutions/spiders/institutions_spider.py
@@ -7,28 +7,31 @@ import json
 class needyDivi(scrapy.Spider):
     name = 'needyDivi'
     start_urls = ['https://www.telelistas.net/mg/divinopolis/associacoes+beneficentes']
-
+    my_list = []
     def parse(self, response):
       _list = response.css('#Content_Regs table')
       name = scrapy.Field()
       address = scrapy.Field()
       print('NEEDY')
-      my_list = []
       for result in _list:
         name = result.css('.text_resultado_ib a::text').extract_first()
         address = result.css('.text_endereco_ib ::text').extract_first()
-        my_list.append({'name': name, 'address': address})
-      for item in my_list:
+        self.my_list.append({'name': name, 'address': address})
+      for item in self.my_list:
         if item['name'] is not None and item['address'] is not None :
           item['name'] = unidecode(item['name'].strip())
           item['address'] = unidecode(item['address'].strip())
 
     
-      filteredList = filter(self.myFunc, my_list)
-      print(self.Remove(filteredList))
+      self.my_list = filter(self.myFunc, self.my_list)
+      print(self.Remove(self.my_list))
+
+      next_url = self.next_page(response)
+      if next_url:
+        yield scrapy.Request(url=next_url, callback=self.parse)
 
       with open('data.json', 'w') as outfile:
-        json.dump(self.Remove(filteredList), outfile) 
+        json.dump(self.Remove(self.my_list), outfile)     
 
     def myFunc(self, x):
       if x['name'] is None or x['address'] is None:
@@ -41,5 +44,9 @@ class needyDivi(scrapy.Spider):
       for num in duplicate: 
         if num not in final_list: 
           final_list.append(num) 
-      return final_list 
-    
+      return final_list
+
+    def next_page(self, response):
+      href = response.css(".text_medio a.resultado_roda::attr('href')").extract_first()
+      url = response.urljoin(href)
+      return url if href else None    
